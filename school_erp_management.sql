55c128f80fa1a1341dcca377f80a334bec380aab
unknown <swarajkumarchaudhary1729@gmail.com>
2022-06-16 17:44:43 +0530

    [ADD] school_erp_management.sql

--- school_erp_management.sql
+++ school_erp_management.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 10, 2022 at 05:37 AM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.12
+-- Generation Time: Jun 16, 2022 at 02:13 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `school_erp_management_db`
+-- Database: `school_erp_management`
 --
 
 -- --------------------------------------------------------
@@ -35,13 +35,6 @@ CREATE TABLE `announcements` (
   `active` int(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='announcement information';
 
---
--- Dumping data for table `announcements`
---
-
-INSERT INTO `announcements` (`aid`, `title`, `descr`, `added_on`, `active`) VALUES
-(1, 'scholarship test cfac', 'lorem ipsum dolor, sit amet consectetur adipisicing elit. doloribus aliquam, harum quis ullam possimus debitis quo alias dolorum dolorem. quia!', '2022-06-09', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -57,14 +50,6 @@ CREATE TABLE `attendance` (
   `total` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='attendance of students as per class and subjects';
 
---
--- Dumping data for table `attendance`
---
-
-INSERT INTO `attendance` (`student_id`, `class_id`, `subject_id`, `present`, `absent`, `total`) VALUES
-(1, 1, 2, 2, 1, 3),
-(1, 1, 1, 1, 1, 2);
-
 -- --------------------------------------------------------
 
 --
@@ -77,13 +62,6 @@ CREATE TABLE `classes` (
   `subject_ids` varchar(255) NOT NULL,
   `active` int(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='subjects in each class';
-
---
--- Dumping data for table `classes`
---
-
-INSERT INTO `classes` (`class_id`, `standard`, `subject_ids`, `active`) VALUES
-(1, 'class 1', '[\"1\",\"2\"]', 1);
 
 -- --------------------------------------------------------
 
@@ -98,8 +76,7 @@ CREATE TABLE `grades` (
   `mid_term_1` int(11) NOT NULL,
   `mid_term_2` int(11) NOT NULL,
   `end_term` int(11) NOT NULL,
-  `marks` int(11) NOT NULL,
-  `total` int(11) NOT NULL
+  `other` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -115,13 +92,6 @@ CREATE TABLE `miscellaneous` (
   `password` varchar(255) NOT NULL,
   `active` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='details of miscellaneous users';
-
---
--- Dumping data for table `miscellaneous`
---
-
-INSERT INTO `miscellaneous` (`miscellaneous_id`, `name`, `email`, `password`, `active`) VALUES
-(1, 'swaraj kumar', 'admin.org@gmail.com', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1);
 
 -- --------------------------------------------------------
 
@@ -143,13 +113,6 @@ CREATE TABLE `students` (
   `active` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='student details';
 
---
--- Dumping data for table `students`
---
-
-INSERT INTO `students` (`student_id`, `name`, `email`, `password`, `class_id`, `phone`, `gender`, `dob`, `doj`, `address`, `active`) VALUES
-(1, 'swaraj kumar', 'swaraj.org@gmail.com', 'e4b40af9e152a905dacf8ff8986bfbfa1ab7600e', 1, '9955075265', 'male', '2022-06-09', '2022-06-09', 'bihar, india', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -166,14 +129,6 @@ CREATE TABLE `subjects` (
   `added_on` date NOT NULL,
   `active` int(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='subjects information';
-
---
--- Dumping data for table `subjects`
---
-
-INSERT INTO `subjects` (`subject_id`, `title`, `descr`, `code`, `credit`, `teacher_id`, `added_on`, `active`) VALUES
-(1, 'art & design', 'lorem ipsum dolor, sit amet consectetur adipisicing elit. doloribus aliquam, harum quis ullam possimus debitis quo alias dolorum dolorem. quia!', 'AD', 1, 1, '2022-06-09', 1),
-(2, 'machine learning', 'lorem ipsum dolor, sit amet consectetur adipisicing elit. doloribus aliquam, harum quis ullam possimus debitis quo alias dolorum dolorem. quia!', 'ML', 2, 2, '2022-06-09', 1);
 
 -- --------------------------------------------------------
 
@@ -193,14 +148,6 @@ CREATE TABLE `teachers` (
   `address` varchar(255) NOT NULL,
   `active` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='teachers details';
-
---
--- Dumping data for table `teachers`
---
-
-INSERT INTO `teachers` (`teacher_id`, `name`, `email`, `password`, `designation`, `phone`, `gender`, `doj`, `address`, `active`) VALUES
-(1, 'lorem ipsum', 'lorem.org@gmail.com', 'b58e92fff5246645f772bfe7a60272f356c0151a', 'assistant professor', '8002046457', 'female', '2022-06-09', 'west bengal, india', 1),
-(2, 'pratyay kuila', 'pratyay.org@gmail.com', '97026e649d3a629d10dee8cbb592a492d0de9862', 'head of under 5', '8002046457', 'male', '2022-06-09', 'chennai, india', 1);
 
 --
 -- Indexes for dumped tables
@@ -250,37 +197,37 @@ ALTER TABLE `teachers`
 -- AUTO_INCREMENT for table `announcements`
 --
 ALTER TABLE `announcements`
-  MODIFY `aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `aid` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `classes`
 --
 ALTER TABLE `classes`
-  MODIFY `class_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `class_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `miscellaneous`
 --
 ALTER TABLE `miscellaneous`
-  MODIFY `miscellaneous_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `miscellaneous_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `subjects`
 --
 ALTER TABLE `subjects`
-  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `teachers`
 --
 ALTER TABLE `teachers`
-  MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
