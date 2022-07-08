ab4b4b2355570525a380d00aa72c507428b175cc
unknown <swarajkumarchaudhary1729@gmail.com>
2022-07-08 17:52:24 +0530

    Updated bootstrap dependencies

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -11,14 +10,18 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Home | ERP Model</title>
-        <!-- Latest compiled and minified CSS & JS or JQuery -->
         <link rel="stylesheet" href="./assets/css/base-styles.css">
-        <?php include("./core-styles-scripts.html") ?>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
 
 <body>
     <div class="container-fluid text-center">
-        <!-- Header -->
+
         <?php include("./includes/header.php") ?>
 
         <div class="wrapper mt-4 mb-5">
@@ -44,17 +47,21 @@
                 <div class="text-center">
                     <p>ANNOUNCEMENTS</p>
                 </div>
-                <ul id="notice" class="text-center">
+                <ul id="notice" class="text-center p-3">
                     <?php
-                    include("./info/announcements.php");
-                    for ($i = 0; $i < sizeof($announcements); $i++) {
-                        $title = ucwords($announcements[$i]["title"]);
-                        $descr = $announcements[$i]["descr"];
-                        $added_on = $announcements[$i]["added_on"];
+                    $announcements_query = "SELECT * FROM announcements WHERE active = 1 ORDER BY announcement_id ASC";
+                    $response = mysqli_query($conn, $announcements_query);
+                    $announcements_details = mysqli_fetch_all($response, MYSQLI_ASSOC);
+
+                    for ($i = 0; $i < sizeof($announcements_details); $i++) {
+                        $title = ucwords($announcements_details[$i]["title"]);
+                        $descr = $announcements_details[$i]["descr"];
+                        $resource = $announcements_details[$i]["resource"];
                         echo "
                             <li class='news-item'>
                                 <p class='font-weight-bold'>$title</p>
                                 <p>$descr</p>
+                                <a href='$resource'>$resource</a>
                             </li>
                             <hr>
                         ";
@@ -116,6 +123,5 @@
 
     </div>
 </body>
-<script src="./assets/js/base-scripts.js"></script>
 
 </html>
